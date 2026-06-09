a04aa3f9ddf317a807bd3b06f86dff77441cad3c
Roy Ratcliffe <roy@ratcliffe.me>
2026-06-09 18:02:33 +0100

    Reorder initialisation of Redis server connection
    Move the initialisation directive for the Redis server
    connection to ensure it is set up correctly after
    defining the connection settings.

--- kmsg.pl
+++ kmsg.pl
@@ -3,14 +3,14 @@
 :- setting(rediscli_host, atom, env('REDISCLI_HOST', localhost), 'Host of the Redis server').
 :- setting(rediscli_port, integer, env('REDISCLI_PORT', 6379), 'Port of the Redis server').
 
-:- initialization(redis_server, after_load).
-
 % Connect to Redis server at Host:Port with version 3 compatibility.
 % This assumes the Redis server is running on the specified port.
 redis_server :-
     setting(rediscli_host, Host),
     setting(rediscli_port, Port),
     redis_server(default, Host:Port, [version(3)]).
+
+:- initialization(redis_server, after_load).
 
 opt_type(v, verbose, boolean).
 opt_type(verbose, verbose, boolean).
